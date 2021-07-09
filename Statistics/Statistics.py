1eb9fc7cc6de5e2a14967df65a74c4996a8e90c0
ierodriguez <ier2@njit.edu>
2021-07-08 22:51:35 -0400

    Feature: setup Statistics.py

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,7 +1,32 @@
 from Calculator.Calculator import Calculator
+from Statistics.Mean import Mean
+from Statistics.Median import Median
+from Statistics.StandardDeviation import StandardDeviation
+from Statistics.Mode import Mode
+from Statistics.Variance import Variance
 
 class Statistics(Calculator):
     data = []
 
     def __init__(self):
         super().__init__()
+
+    def mean(self, data):
+        self.result = Mean.mean(data)
+        return self.result
+
+    def median(self, data):
+        self.result = Median.median(data)
+        return self.result
+
+    def mode(self, data):
+        self.result = Mode.mode(data)
+        return self.result
+
+    def variance(self, data):
+        self.result = Variance.variance(data)
+        return self.result
+
+    def standardDeviation(self, data):
+        self.result = StandardDeviation.standardDeviation(data)
+        return self.result
